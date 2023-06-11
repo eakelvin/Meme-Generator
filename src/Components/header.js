a5af6e9583b84b23a8eb3ee3aa4e7844380aa06c
Kelvin Akaba <akabakelvin@gmail.com>
2023-06-11 15:50:24 +0000

    designed the structure

--- src/Components/header.js
+++ src/Components/header.js
@@ -7,6 +7,7 @@ export default function Header() {
         <img 
             src="./images/troll-face.png" 
             className="header--image"
+            alt=""
         />
         <h2 className="header--title">Meme Generator</h2>
         <h4 className="header--project">React Course - Project 3</h4>
